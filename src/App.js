360a6008a5f69a230bf7653b48bbe34877511f76
Nathan Roberts <nkroberts01@gmail.com>
2023-08-08 11:13:52 -0600

    Created react structure, started initial layout

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from './components/navbar'
+import Home from './pages/home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
